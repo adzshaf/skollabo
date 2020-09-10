b0b40d3b707c215b89be2ec8d08a832a7f5ef6e8
Shafiya Adzhani <adz.arsym@gmail.com>
2020-09-11 07:32:58 +0800

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,33 @@ module.exports = {
       display: ["Ubuntu", "sans-serif"],
       body: ["Ubuntu", "sans-serif"],
     },
+    colors: {
+      blue: {
+        default: "#4856DF",
+        bright: "#3EC1F3",
+        cornflower: "#66B0F0",
+      },
+      mustard: {
+        default: "#FFBE21",
+        bright: "#FFCF2D",
+      },
+      pink: {
+        default: "#FFB7D5",
+        dark: "#E57281",
+        purple: "#73325A",
+      },
+      red: {
+        soft: "#A0505A",
+        orange: "#FE5244",
+        default: "#FE5244",
+      },
+      green: {
+        blueish: "#3FB1B5",
+        default: "#D0F0E4",
+      },
+      brick: "#A15842",
+      cream: "#F5EBDD",
+    },
     extend: {},
   },
   variants: {},
