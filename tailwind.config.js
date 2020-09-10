ffeeddc1a4bc97acf74d60dff7c5b946b68611d1
Shafiya Adzhani <adz.arsym@gmail.com>
2020-09-11 07:01:18 +0800

    add font

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ["Ubuntu", "sans-serif"],
+      body: ["Ubuntu", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
